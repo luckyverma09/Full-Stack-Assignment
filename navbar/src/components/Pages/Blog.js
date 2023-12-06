e90a61d8a96fffa18fbf5fa0004455c3422eef0b
Lucky Verma <117267237+luckyverma09@users.noreply.github.com>
2023-12-06 14:33:54 +0530

    Delete navbar directory

--- navbar/src/components/Pages/Blog.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Blog = () => {
-  return (
-    <div>
-      <h1>This is Blog Page.</h1>
-    </div>
-  );
-};
--- navbar/src/components/Pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Home = () => {
-  return (
-    <div>
-      <h1>This is Homepage.</h1>
-    </div>
-  );
-};
